ea08fa7ce3f9e31029a082db02877e608d1e5c79
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2020-10-26 12:38:53 +0100

    Add test application

---
+++ tests/Application/src/Entity/Channel/Channel.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' No Commerce plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Entity\Channel;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\Channel as BaseChannel;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_channel")
+ * @ORM\AssociationOverrides({
+ *     @ORM\AssociationOverride(name="baseCurrency",
+ *         joinColumns=@ORM\JoinColumn(
+ *             name="base_currency_id", referencedColumnName="id", nullable=true
+ *         )
+ *     )
+ * })
+ */
+class Channel extends BaseChannel
+{
+}

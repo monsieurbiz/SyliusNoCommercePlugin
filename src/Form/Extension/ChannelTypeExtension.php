7a3737ec4f5aec8594c6c8bfcfeb51c9117da139
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-09 18:08:50 +0100

    Make getExtendedTypes static to be compatible to Symfony 5

--- src/Form/Extension/ChannelTypeExtension.php
+++ src/Form/Extension/ChannelTypeExtension.php
@@ -35,7 +35,7 @@ final class ChannelTypeExtension extends AbstractTypeExtension
         ;
     }
 
-    public function getExtendedTypes(): array
+    public static function getExtendedTypes(): array
     {
         return [ChannelType::class];
     }

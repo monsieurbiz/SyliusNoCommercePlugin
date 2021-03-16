ebde7b9913517b7b4dd685062c083334a687754e
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-16 15:30:43 +0100

    Add config model to be able to use plugin configuration

---
+++ src/Model/Config.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' No Commerce plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusNoCommercePlugin\Model;
+
+final class Config implements ConfigInterface
+{
+    private $config = [];
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function getAllowCustomers(): bool
+    {
+        return (bool) $this->config['allow_customers'] ?: false;
+    }
+}

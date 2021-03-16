35ba23cf2c56394cdc12d2c5462503a5c3836e00
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-16 15:31:52 +0100

    Don't remove customer routes if customers allowed in config

--- src/Kernel/SyliusNoCommerceKernelTrait.php
+++ src/Kernel/SyliusNoCommerceKernelTrait.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusNoCommercePlugin\Kernel;
 
+use MonsieurBiz\SyliusNoCommercePlugin\Model\Config;
+use MonsieurBiz\SyliusNoCommercePlugin\Model\ConfigInterface;
 use MonsieurBiz\SyliusNoCommercePlugin\Routing\RouteCollectionBuilder;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -29,9 +31,21 @@ trait SyliusNoCommerceKernelTrait
      */
     public function loadRoutes(LoaderInterface $loader)
     {
-        $routes = new RouteCollectionBuilder($loader);
+        $routes = new RouteCollectionBuilder($this->getConfig(), $loader);
         $this->configureRoutes($routes);
 
         return $routes->build();
+    }
+
+    /**
+     * Create a NoCommerce Config object.
+     *
+     * @return ConfigInterface
+     */
+    private function getConfig(): ConfigInterface
+    {
+        return new Config(
+            (array) $this->container->getParameter('monsieurbiz_sylius_nocommerce.config') ?? []
+        );
     }
 }

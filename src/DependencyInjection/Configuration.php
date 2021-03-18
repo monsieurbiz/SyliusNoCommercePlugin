bb39c41bfc6285b2e4c42393566c9e7396e16e90
Maxime Huran <m.huran@monsieurbiz.com>
2021-03-18 14:45:26 +0100

    Remove BC layer for symfony/config 4.1 and older

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -24,12 +24,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('monsieurbiz_sylius_nocommerce');
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('monsieur_biz_sylius_search');
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
